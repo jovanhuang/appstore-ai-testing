43956cef16518c8bbd9cc590ce4d9fa0f31f1531
jovanhuang <jhtc98@hotmail.com>
2024-01-16 10:58:57 +0800

    Test create new branch and commit changes

--- back-end/tests/unit/apis/test_auth.py
+++ back-end/tests/unit/apis/test_auth.py
@@ -1,3 +1,8 @@
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
+
+# Uncommitted changes. Working halfway! 
+# Stash this as I want to create a new branch and apply this stash there!
+
+# Add more changes
